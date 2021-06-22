f6e9d03f3ae452979f6d4f964ca0869a5fd348e0
tony.ic.lin <tony.ic.lin@deltaww.com>
2021-06-22 23:51:36 +0800

    basic

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <Three001/>
+</template>
+
+<script>
+import Three001 from './components/Three001.vue'
+
+
+export default  {
+  name: 'App',
+  components: {
+    Three001
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
